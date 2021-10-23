c661b98652d369b14f9ae035ea04254334831659
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-10-23 13:35:00 +0700

    web

---
+++ web/application/controllers/API/Auth.php
@@ -0,0 +1,54 @@
+<?php
+
+use chriskacerguis\RestServer\RestController;
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends RestController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function index_post()
+    {
+        $user = $this->input->post('username');
+        $password = $this->input->post('password');
+
+        $data = $this->db->get_where('user', ['username' => $user, 'status' => 'on'])->row_array();
+        if($data){
+            if($data['password'] == $password){
+                $this->response(
+                    [
+                        'status' => 'true',
+                        'pesan' => 'Anda Berhasil Login',
+                        'data' => $data
+                    ],
+                    RestController::HTTP_OK
+                );
+            }else{
+                $this->response(
+                    [
+                        'status' => 'false',
+                        'pesan' => 'Password Yang Anda Masukan Salah',
+                        'data' => 'null'
+                    ],
+                    RestController::HTTP_FORBIDDEN
+                );
+            }
+            
+        }else{
+            $this->response(
+                [
+                    'status' => 'false',
+                    'pesan' => 'Username Tidak Ditemukan',
+                    'data' => 'null'
+                ],
+                RestController::HTTP_FORBIDDEN
+            );
+        }
+    }
+}
+
+/* End of file Auth.php and path /application/controllers/Auth/Auth.php */
