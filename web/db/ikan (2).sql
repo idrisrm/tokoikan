483770738d7c62022d25daab6bc10f00eadefbc7
Dimas1711 <pdimas429@gmail.com>
2021-10-26 23:05:51 +0700

    Create ikan (2).sql

---
+++ web/db/ikan (2).sql
@@ -0,0 +1,270 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Okt 2021 pada 18.00
+-- Versi server: 10.4.18-MariaDB
+-- Versi PHP: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ikan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` varchar(6) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `id_otlet` int(11) NOT NULL,
+  `nama_barang` text NOT NULL,
+  `stok` int(11) NOT NULL COMMENT 'per kg',
+  `harga` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `status` enum('on','off') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `id_kategori`, `id_otlet`, `nama_barang`, `stok`, `harga`, `created_at`, `status`) VALUES
+('1', 2, 0, 'pakan Sapi besar', 4, 44444, '2021-10-26 04:27:59', 'on');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `detail_penjualan`
+--
+
+CREATE TABLE `detail_penjualan` (
+  `id_detail` int(11) NOT NULL,
+  `id_penjualan` varchar(10) NOT NULL,
+  `id_barang` varchar(6) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `total_harga` int(11) NOT NULL,
+  `catatan` text DEFAULT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `hutang`
+--
+
+CREATE TABLE `hutang` (
+  `no_ktp` varchar(16) NOT NULL,
+  `nama_penghutang` text NOT NULL,
+  `foto_ktp` text NOT NULL,
+  `total_hutang` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `status` int(1) NOT NULL COMMENT '0 = belum konfirmasi, 1 = bisa hutang, 2 = tidak bisa hutang, 3 = lunas'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `hutang`
+--
+
+INSERT INTO `hutang` (`no_ktp`, `nama_penghutang`, `foto_ktp`, `total_hutang`, `created_at`, `updated_at`, `status`) VALUES
+('123', 'tes', 'uploads/ktp/2JxyScreenshot_1.png', 0, '2021-10-26 14:26:11', '2021-10-26 14:26:11', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` text NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`, `created_at`) VALUES
+(1, 'Pakan Ikan', '2021-10-26 04:28:34'),
+(2, 'Pakan sapi\r\n', '2021-10-26 04:28:34');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `notifikasi`
+--
+
+CREATE TABLE `notifikasi` (
+  `id_notif` varchar(15) NOT NULL,
+  `id_tujuan` int(11) NOT NULL,
+  `judul` text NOT NULL,
+  `deskripsi` text NOT NULL,
+  `status` int(1) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `otlet`
+--
+
+CREATE TABLE `otlet` (
+  `id_otlet` int(11) NOT NULL,
+  `wilayah` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `otlet`
+--
+
+INSERT INTO `otlet` (`id_otlet`, `wilayah`) VALUES
+(1, 'Jember'),
+(2, 'Situbondo'),
+(3, 'Bali');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` varchar(10) NOT NULL,
+  `id_penjual` int(11) NOT NULL,
+  `subtotal` int(11) NOT NULL,
+  `status` int(1) NOT NULL COMMENT '1 = bayar, 2 = hutang',
+  `ktp_penghutang` varchar(16) DEFAULT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `id_otlet` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `bagian` enum('admin','karyawan') NOT NULL,
+  `status` enum('on','off') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `id_otlet`, `nama`, `username`, `password`, `bagian`, `status`) VALUES
+(1, 1, 'idris', 'idris', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'on'),
+(2, 2, 'Anggi Anggraeniii', 'anggi', '21232f297a57a5a743894a0e4a801fc3', 'karyawan', 'on');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indeks untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `id_penjualan` (`id_penjualan`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indeks untuk tabel `hutang`
+--
+ALTER TABLE `hutang`
+  ADD PRIMARY KEY (`no_ktp`);
+
+--
+-- Indeks untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indeks untuk tabel `notifikasi`
+--
+ALTER TABLE `notifikasi`
+  ADD PRIMARY KEY (`id_notif`),
+  ADD KEY `id_tujuan` (`id_tujuan`);
+
+--
+-- Indeks untuk tabel `otlet`
+--
+ALTER TABLE `otlet`
+  ADD PRIMARY KEY (`id_otlet`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`),
+  ADD KEY `id_penjual` (`id_penjual`),
+  ADD KEY `ktp_penghutang` (`ktp_penghutang`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `detail_penjualan`
+--
+ALTER TABLE `detail_penjualan`
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `otlet`
+--
+ALTER TABLE `otlet`
+  MODIFY `id_otlet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
