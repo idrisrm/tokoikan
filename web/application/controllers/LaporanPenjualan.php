0cbd8d78568fbb288402c5237bd1cf68cd089b3b
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-11-01 14:31:45 +0700

    Update LaporanPenjualan.php

--- web/application/controllers/LaporanPenjualan.php
+++ web/application/controllers/LaporanPenjualan.php
@@ -10,7 +10,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class LaporanPenjualan extends CI_Controller
 {
-    
+
     public function __construct()
     {
         parent::__construct();
@@ -22,35 +22,47 @@ class LaporanPenjualan extends CI_Controller
         $sampaitanggal = $this->input->post('sampaitanggal');
         $metode = $this->input->post('metode');
 
+        $nama = '';
+        if ($metode == 1) {
+            $nama = 'Cash';
+        } else {
+            $nama = 'Hutang';
+        }
 
-
-        $this->db->where(['status' => $metode]);
-        $this->db->where('created_at BETWEEN "' . date('Y-m-d', strtotime($daritanggal)) . '" and "' . date('Y-m-d', strtotime($sampaitanggal)) . '"');
+        $this->db->join('detail_penjualan', 'detail_Penjualan.id_penjualan = penjualan.id_penjualan');
+        $this->db->join('barang', 'barang.id_barang = detail_penjualan.id_barang');
+        $this->db->join('user', 'user.id = penjualan.id_penjual');
+        $this->db->join('otlet', 'otlet.id_otlet = penjualan.id_otlet');
+        $this->db->where(['penjualan.status' => $metode]);
+        $this->db->where('penjualan.created_at BETWEEN "' . date('Y-m-d', strtotime($daritanggal)) . '" and "' . date('Y-m-d', strtotime($sampaitanggal)) . '"');
         $data = $this->db->get('penjualan')->result_array();
 
+        // var_dump($data);die;
         $spreadsheet = new Spreadsheet();
         $sheet = $spreadsheet->getActiveSheet();
         $sheet->setCellValue('A1', 'No');
-        $sheet->setCellValue('B1', 'subtotal');
-        // $sheet->setCellValue('C1', 'Kelas');
-        // $sheet->setCellValue('D1', 'Jenis Kelamin');
-        // $sheet->setCellValue('E1', 'Alamat');
+        $sheet->setCellValue('B1', 'Nama Karyawan');
+        $sheet->setCellValue('C1', 'Nama Barang');
+        $sheet->setCellValue('D1', 'Jumlah');
+        $sheet->setCellValue('E1', 'Harga');
+        $sheet->setCellValue('F1', 'Total Harga');
 
         $no = 1;
         $x = 2;
         foreach ($data as $data) {
             $sheet->setCellValue('A' . $x, $no++);
-            $sheet->setCellValue('B' . $x, $data['sub_total']);
-            // $sheet->setCellValue('C' . $x, $row->kelas);
-            // $sheet->setCellValue('D' . $x, $row->jenis_kelamin);
-            // $sheet->setCellValue('E' . $x, $row->alamat);
+            $sheet->setCellValue('B' . $x, $data['nama']);
+            $sheet->setCellValue('C' . $x, $data['nama_barang']);
+            $sheet->setCellValue('D' . $x, $data['qty']);
+            $sheet->setCellValue('E' . $x, $data['harga']);
+            $sheet->setCellValue('F' . $x, $data['total_harga']);
             $x++;
         }
         $writer = new Xlsx($spreadsheet);
-        $filename = 'laporan-Penjualan';
+        $filename = 'Laporan-Penjualan-' . $nama;
 
         header('Content-Type: application/vnd.ms-excel');
-        header('Content-Disposition: attachment;filename="' . $filename . '.xlsx"');
+        header('Content-Disposition: attachment;filename="' . $filename . '.xls"');
         header('Cache-Control: max-age=0');
 
         $writer->save('php://output');
