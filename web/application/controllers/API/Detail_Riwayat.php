438d841850b481dc0d537937e89db34d4932276e
Dimas1711 <pdimas429@gmail.com>
2021-11-02 10:36:49 +0700

    update riwayat penjualan

--- web/application/controllers/API/Detail_Riwayat.php
+++ web/application/controllers/API/Detail_Riwayat.php
@@ -14,13 +14,17 @@ class Detail_Riwayat extends RestController
     public function index_get()
     {
         $id = $this->get('id_penjualan');
-        $data = $this->db->query("SELECT * FROM penjualan, detail_penjualan WHERE penjualan.id_penjualan = detail_penjualan.id_penjualan AND penjualan.id_penjualan = '$id'")->result_array();
+        $data = $this->db->query("SELECT penjualan.id_penjualan, penjualan.created_at, penjualan.subtotal,  detail_penjualan.qty, detail_penjualan.total_harga, detail_penjualan.catatan, barang.nama_barang FROM penjualan, detail_penjualan, barang WHERE penjualan.id_penjualan = detail_penjualan.id_penjualan AND detail_penjualan.id_barang = barang.id_barang AND penjualan.id_penjualan = '$id'")->result_array();
         if ($data) {
+            foreach ($data as $d) {
+                $subtotal = $d['subtotal'];
+            }
             $this->response(
                 [
                     'status' => true,
                     'pesan' => 'Detail riwayat transaksi ditemukan',
-                    'data' => $data
+                    'subtotal' => $subtotal,
+                    'data' => $data,
                 ],
                 RestController::HTTP_OK
             );
