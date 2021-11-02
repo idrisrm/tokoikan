7cb6a7f4ba60d7756f684d5da4aba10d3ed0f19b
Dimas1711 <pdimas429@gmail.com>
2021-11-02 10:43:56 +0700

    Create Riwayat_Admin.php

---
+++ web/application/controllers/API/Riwayat_Admin.php
@@ -0,0 +1,37 @@
+<?php
+
+use chriskacerguis\RestServer\RestController;
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Riwayat_Admin extends RestController
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function index_get()
+    {
+        $data = $this->db->query("SELECT penjualan.*, otlet.wilayah FROM penjualan, otlet WHERE penjualan.id_otlet = otlet.id_otlet AND penjualan.status != 0")->result_array();
+        if ($data) {
+            $this->response(
+                [
+                    'status' => true,
+                    'pesan' => 'Riwayat transaksi ditemukan',
+                    'data' => $data
+                ],
+                RestController::HTTP_OK
+            );
+        } else {
+            $this->response(
+                [
+                    'status' => false,
+                    'pesan' => 'Tidak ada riwayat transaksi',
+                    'data' => null
+                ],
+                RestController::HTTP_NOT_FOUND
+            );
+        }
+    }
+}
