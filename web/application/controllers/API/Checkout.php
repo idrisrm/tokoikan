33ae3bf34a785aceb7886b355376df00db7a3034
Dimas1711 <pdimas429@gmail.com>
2021-11-10 18:03:33 +0700

    Update Checkout.php

--- web/application/controllers/API/Checkout.php
+++ web/application/controllers/API/Checkout.php
@@ -56,7 +56,7 @@ class Checkout extends RestController
         } else {
             $hutang = $this->db->query("SELECT * FROM hutang WHERE no_ktp = '$no_ktp'")->row_array();
             $banyakhutang = $hutang['total_hutang'] + $subtotal;
-            if ($banyakhutang <= 10000000) {
+            if ($banyakhutang < 10000000) {
                 //ubah stok
                 $ubahstok = $this->db->query("SELECT * FROM detail_penjualan WHERE id_penjualan = '$id_penjualan'")->result_array();
                 foreach ($ubahstok as $us) {
@@ -98,7 +98,7 @@ class Checkout extends RestController
                         RestController::HTTP_FORBIDDEN
                     );
                 }
-            } else {
+            } else if ($banyakhutang > 10000000) {
                 $this->response(
                     [
                         'status' => false,
@@ -106,6 +106,49 @@ class Checkout extends RestController
                     ],
                     RestController::HTTP_FORBIDDEN
                 );
+            } else {
+                //ubah stok
+                $ubahstok = $this->db->query("SELECT * FROM detail_penjualan WHERE id_penjualan = '$id_penjualan'")->result_array();
+                foreach ($ubahstok as $us) {
+                    $id_barang = $us['id_barang'];
+                    $qty = $us['qty'];
+                    $barang = $this->db->query("SELECT * FROM barang WHERE id_barang = '$id_barang'")->result_array();
+                    foreach ($barang as $b) {
+                        $data = [
+                            'stok' => $b['stok'] - $qty,
+                        ];
+                        $this->ApiModel->ubah($data, $id_barang, 'id_barang', 'barang');
+                    }
+                }
+                $datapenjualan = [
+                    'subtotal' => $subtotal,
+                    'status' => $status,
+                    'ktp_penghutang' => $no_ktp,
+                ];
+                //ubah di hutang
+                $datahutang = [
+                    'total_hutang' => $hutang['total_hutang'] + $subtotal,
+                    'status' => 2,
+                ];
+                $querypenjualan = $this->ApiModel->ubah($datapenjualan, $id_penjualan, 'id_penjualan', 'penjualan');
+                $queryhutang = $this->ApiModel->ubah($datahutang, $no_ktp, 'no_ktp', 'hutang');
+                if ($querypenjualan && $queryhutang) {
+                    $this->response(
+                        [
+                            'status' => true,
+                            'pesan' => 'Berhasil checkout',
+                        ],
+                        RestController::HTTP_OK
+                    );
+                } else {
+                    $this->response(
+                        [
+                            'status' => false,
+                            'pesan' => 'Gagal checkout terjadi kesalahan',
+                        ],
+                        RestController::HTTP_FORBIDDEN
+                    );
+                }
             }
         }
     }
