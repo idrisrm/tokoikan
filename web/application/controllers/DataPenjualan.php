088ba9d5f7df045b662e88ae2c52c7ec77093a3c
Dimas1711 <pdimas429@gmail.com>
2021-11-20 23:31:20 +0700

    Update DataPenjualan.php

--- web/application/controllers/DataPenjualan.php
+++ web/application/controllers/DataPenjualan.php
@@ -12,28 +12,27 @@ class DataPenjualan extends CI_Controller
     public function jember()
     {
         $data['jember'] = $this->db->join('detail_penjualan', 'detail_penjualan.id_penjualan = penjualan.id_penjualan')->join('barang', 'barang.id_barang = detail_penjualan.id_barang')->get_where('penjualan', ['penjualan.id_otlet' => '1', 'penjualan.pengajuan' => ''])->result_array();
-        
+
         // var_dump($data);die;
         $this->load->view('DataPenjualan/jember', $data);
     }
     public function situbondo()
     {
         $data['situbondo'] = $this->db->join('detail_penjualan', 'detail_penjualan.id_penjualan = penjualan.id_penjualan')->join('barang', 'barang.id_barang = detail_penjualan.id_barang')->get_where('penjualan', ['penjualan.id_otlet' => '2', 'penjualan.pengajuan' => ''])->result_array();
-        
+
         // var_dump($data);die;
         $this->load->view('DataPenjualan/situbondo', $data);
     }
     // public function datasitubondo()
     // {
     //     $data = $this->db->join('detail_penjualan', 'detail_penjualan.id_penjualan = penjualan.id_penjualan')->join('barang', 'barang.id_barang = detail_penjualan.id_barang')->get_where('penjualan', ['penjualan.id_otlet' => '2'])->result_array();
-        
+
     //     echo json_encode($data);
     // }
     public function bali()
     {
-        $data['bali'] = $this->db->join('detail_penjualan', 'detail_penjualan.id_penjualan = penjualan.id_penjualan')->join('barang', 'barang.id_barang = detail_penjualan.id_barang')->get_where('penjualan', ['penjualan.id_otlet' => '3', 'penjualan.pengajuan' => ''])->result_array();
-        
-        // var_dump($data);die;
+        // $data['bali'] = $this->db->join('detail_penjualan', 'detail_penjualan.id_penjualan = penjualan.id_penjualan')->join('barang', 'barang.id_barang = detail_penjualan.id_barang')->get_where('penjualan', ['penjualan.id_otlet' => '3', 'penjualan.pengajuan' => ''])->result_array();
+        $data['bali'] = $this->db->query("SELECT barang.nama_barang, barang.harga, barang.satuan, detail_penjualan.*, penjualan.* FROM detail_penjualan, penjualan, barang WHERE penjualan.id_penjualan = detail_penjualan.id_penjualan AND detail_penjualan.id_barang = barang.id_barang AND penjualan.id_otlet = '3' AND penjualan.pengajuan = ''")->result_array();
         $this->load->view('DataPenjualan/bali', $data);
     }
 }
