d3996bb68e1071003926a41db3f038f896d84b4b
idrisrm <55520214+idrisrm@users.noreply.github.com>
2021-12-01 17:33:02 +0700

    Update sidebar.php

--- web/application/views/_partials/sidebar.php
+++ web/application/views/_partials/sidebar.php
@@ -8,55 +8,78 @@
                     <div class="nav-link-icon"><i data-feather="activity"></i></div>
                     Dashboard
                 </a>
-                <div class="sidenav-menu-heading">Menu Pengaduan</div>
+                <div class="sidenav-menu-heading">Menu Transaksi</div>
                 <a class="nav-link collapsed" href="javascript:void(0);" data-toggle="collapse" data-target="#collapsePengaduan" aria-expanded="false" aria-controls="collapsePengaduan">
                     <div class="nav-link-icon"><i data-feather="file-text"></i></div>
-                    Transaksi
+                    Penjualan
                     <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
                 <div class="collapse show" id="collapsePengaduan" data-parent="#accordionSidenav">
                     <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
                         <a class="nav-link" href="<?= base_url('DataPenjualan/jember') ?>">
                             <span class="badge badge-success mr-1">  </span>
-                            Penjualan
+                            Penjualan Produk
                             <!-- <span class="badge badge-primary-soft text-primary ml-auto">2</span> -->
                         </a>
                         <a class="nav-link" href="<?= base_url('RevisiTransaksi') ?>">
                             <span class="badge badge-cyan mr-1">  </span>
-                            Revisi Transaksi
+                            Revisi Penjualan
                             <!-- <span class="badge badge-primary-soft text-primary ml-auto">2</span> -->
                         </a>
-                        <a class="nav-link" href="<?= base_url('DataPenghutang/PengajuanHutang') ?>">
+                        <!-- <a class="nav-link" href="<?= base_url('DataPenghutang/PengajuanHutang') ?>">
                             <span class="badge badge-warning mr-1">  </span>
                             Pengajuan Hutang
+                        </a> -->
+                    </nav>
+                </div>
+                <a class="nav-link collapsed" href="javascript:void(0);" data-toggle="collapse" data-target="#collapsePengaduan" aria-expanded="false" aria-controls="collapsePengaduan">
+                    <div class="nav-link-icon"><i data-feather="file-text"></i></div>
+                    Transaksi Keuangan
+                    <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse show" id="collapsePengaduan" data-parent="#accordionSidenav">
+                    <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
+                        <a class="nav-link" href="<?= base_url('DataPenjualan/jember') ?>">
+                            <span class="badge badge-success mr-1">  </span>
+                            Arus kas
                             <!-- <span class="badge badge-primary-soft text-primary ml-auto">2</span> -->
                         </a>
-                        <!-- <a class="nav-link" href="<?= base_url('Pengaduan/data_tindak_lanjut') ?>">
-                            <span class="badge badge-blue mr-1">  </span>
-                            Tindak Lanjut
-                            <span class="badge badge-primary-soft text-primary ml-auto">2</span>
+                        <a class="nav-link" href="<?= base_url('RevisiTransaksi') ?>">
+                            <span class="badge badge-cyan mr-1">  </span>
+                            Hutang Ke Supplier
+                            <!-- <span class="badge badge-primary-soft text-primary ml-auto">2</span> -->
                         </a>
-                        <a class="nav-link" href="<?= base_url('Pengaduan/data_selesai') ?>">
-                            <span class="badge badge-success mr-1">  </span>
-                            Selesai
-                            <span class="badge badge-primary-soft text-primary ml-auto">2</span>
+                        <a class="nav-link" href="<?= base_url('DataPenghutang/') ?>">
+                            <span class="badge badge-warning mr-1">  </span>
+                            Piutang Pelanggan
+                            <!-- <span class="badge badge-primary-soft text-primary ml-auto">2</span> -->
                         </a>
-                        <a class="nav-link" href="<?= base_url('Pengaduan/data_tolak') ?>">
-                            <span class="badge badge-indigo mr-1">  </span>
-                            Tolak
-                            <span class="badge badge-primary-soft text-primary ml-auto">2</span>
-                        </a>
-                        <a class="nav-link" href="<?= base_url('Pengaduan/data_arsip') ?>">
-                            Arsip
-                            <span class="badge badge-primary-soft text-primary ml-auto">2</span>
-                        </a> -->
                     </nav>
                 </div>
 
                 <div class="sidenav-menu-heading">Master Data</div>
-                <a class="nav-link" href="<?= base_url('DataBarang/jember') ?>">
+                <a class="nav-link collapsed" href="javascript:void(0);" data-toggle="collapse" data-target="#collapseproduk" aria-expanded="false" aria-controls="collapseproduk">
                     <div class="nav-link-icon"><i data-feather="box"></i></div>
-                    Data Barang
+                    Produk
+                    <div class="sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                </a>
+                <div class="collapse" id="collapseproduk" data-parent="#accordionSidenav">
+                    <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
+                        <a class="nav-link" href="<?= base_url('DataBarang/jember') ?>">
+                            <span class="badge badge-success mr-1">  </span>
+                            Stok Produk
+                            <!-- <span class="badge badge-primary-soft text-primary ml-auto">2</span> -->
+                        </a>
+                        <a class="nav-link" href="<?= base_url('RevisiTransaksi') ?>">
+                            <span class="badge badge-cyan mr-1">  </span>
+                            Peringkat Produk Yang terjual
+                            <!-- <span class="badge badge-primary-soft text-primary ml-auto">2</span> -->
+                        </a>
+                    </nav>
+                </div>
+                <a class="nav-link" href="#">
+                    <div class="nav-link-icon"><i data-feather="shopping-bag"></i></div>
+                    Pembelian Produk
                 </a>
                 <a class="nav-link" href="<?= base_url('DataPenghutang') ?>">
                     <div class="nav-link-icon"><i data-feather="user"></i></div>
